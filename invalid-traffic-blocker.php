e192bbd73edeb6470b36299dc3c8a6de907122e6
9jaDevO <macruben03@gmail.com>
2025-04-02 12:49:56 +0100

    Add short description to Invalid Traffic Blocker plugin

--- invalid-traffic-blocker.php
+++ invalid-traffic-blocker.php
@@ -4,6 +4,7 @@
  * Plugin Name: Invalid Traffic Blocker
  * Plugin URI: https://github.com/9jaDevo/IPHub-Blocker
  * Description: Blocks unwanted traffic using the IPHub.info API to protect AdSense publishers from invalid traffic.
+ * Short Description: Protect your site from invalid traffic by blocking suspicious IPs using the IPHub.info API.
  * Version: 1.2
  * Author: Michael Akinwumi
  * Author URI: https://michaelakinwumi.com/
